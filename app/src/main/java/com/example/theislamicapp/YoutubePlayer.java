e8c38d27eae1aff6de2302da328e83dea7521299
daudbaig <daudbaig91@gmail.com>
2022-10-24 00:26:36 +0100

    tv first 5

--- app/src/main/java/com/example/theislamicapp/YoutubePlayer.java
+++ app/src/main/java/com/example/theislamicapp/YoutubePlayer.java
@@ -1,13 +1,25 @@
 package com.example.theislamicapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.YouTubePlayerTracker;
@@ -15,6 +27,8 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.utils.YouTube
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -26,8 +40,9 @@ public class YoutubePlayer extends FragmentActivity {
     String keyYoutube = "UDvh63xHVa0";
     String pref = "qurantimes";
     Float currentSeconds;
+    YouTubePlayer player2 = null;
 
-
+    String status = "PLAYING";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +59,7 @@ public class YoutubePlayer extends FragmentActivity {
         getLifecycle().addObserver(youTubePlayerView);
 
         youTubePlayerView.initialize(new AbstractYouTubePlayerListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onReady(@NonNull YouTubePlayer player) {
                 Float duration = 0f;
@@ -55,7 +71,7 @@ public class YoutubePlayer extends FragmentActivity {
                                     .getString(keyYoutube,""));
                 }
 
-
+                player2 = player;
                 youtubePlayerSave = player;
                 YouTubePlayerUtils.loadOrCueVideo(
                         player,
@@ -63,11 +79,22 @@ public class YoutubePlayer extends FragmentActivity {
                         keyYoutube,
                         duration
                 );
+                //youtubePlayerSave.seekTo(20f);
+
+
+
             }
             @Override
             public void onCurrentSecond(@NotNull YouTubePlayer youTubePlayer, float v) {
                 currentSeconds = v;
             }
+
+            @Override
+            public void onStateChange(@NonNull YouTubePlayer youTubePlayer, @NonNull PlayerConstants.PlayerState state){
+                Log.d("stringsa",state.toString());
+                status = state.toString();
+            }
+
         });
 
 
@@ -87,6 +114,7 @@ public class YoutubePlayer extends FragmentActivity {
     }
     @Override
     protected void onPause() {
+
         SavedContent();
         super.onPause();
     }
@@ -104,6 +132,35 @@ public class YoutubePlayer extends FragmentActivity {
             editor.putString(String.valueOf(keyYoutube), String.valueOf(currentSeconds - 2.0));
             editor.apply();
         }
+
+    }
+int i = 0;
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event){
+        Log.d("string121", String.valueOf(i++));
+        if (event.getKeyCode()== KeyEvent.KEYCODE_DPAD_CENTER ||event.getKeyCode()== KeyEvent.KEYCODE_DPAD_UP) {
+            if (status.equals("PLAYING")) {
+                player2.pause();
+            } else {
+                player2.play();
+            }
+        }
+        else if (event.getKeyCode()== KeyEvent.KEYCODE_DPAD_LEFT)
+                player2.seekTo(currentSeconds-=7);
+
+        else if (event.getKeyCode()== KeyEvent.KEYCODE_DPAD_RIGHT)
+                player2.seekTo(currentSeconds+=7);
+        else if (event.getKeyCode()== KeyEvent.KEYCODE_BACK){
+            finish();
+        }
+        return true;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void run(View view){
+
 
     }
 }
